1693ec57f4e560bb5cc4f1f04cd3650d8fb54b81
mattrfk <mattrfk@gmail.com>
2018-11-23 17:52:17 -0800

    move around with arrow keys

--- script.js
+++ script.js
@@ -4,10 +4,32 @@ function l(msg){console.log(msg)}
 window.onload = function() {
 	grid = gebi("grid")
 	generateGrid(grid)
+	document.addEventListener('keypress', (event) => {
+		let c = document.activeElement
+		if(c.type !== "textarea") { return }
+		let id = c.getAttribute("id").split("")
+		let x = Number(id[0])
+		let y = Number(id[1])
+		switch(event.key) {
+			case "ArrowUp":
+				if(y > 0) y -= 1
+				break
+			case "ArrowRight":
+				if(x < 8) x += 1
+				break
+			case "ArrowDown":
+				if(y < 8) y += 1
+				l("here")
+				break
+			case "ArrowLeft":
+				if(x > 0) x -= 1
+		}
+		let n = gebi(""+x+y)
+		n.focus()
+	})
 }
 
 //TODO: only one digit allowed
-//TODO: intercept arrow keys
 
 function generateGrid(grid){
 	let t = document.createElement("table")
@@ -19,7 +41,7 @@ function generateGrid(grid){
 			let td = document.createElement("td")
 			var textArea = document.createElement("textarea")
 			var newContent = document.createTextNode(i)
-			textArea.setAttribute("id", ""+i+n)
+			textArea.setAttribute("id", ""+n+i)
 			textArea.setAttribute("contenteditable", "true")
 			textArea.onfocus = function(e) {
 				e.target.select()
@@ -27,6 +49,14 @@ function generateGrid(grid){
 			textArea.appendChild(newContent)
 			td.appendChild(textArea)
 			row.appendChild(td)
+
+			// there's probably a better way to do this
+			let divider = "5px "
+			let l = (n === 0 || n === 3 || n === 6) ? divider : "0 "
+			let t = (i === 0 || i === 3 || i === 6) ? divider : "0 "
+			let b = (i === 8) ? divider : "0 "
+			let r = (n === 8) ? divider : "0 "
+			td.setAttribute("style", "border-width:"+t+r+b+l+";")
 		}
 	}
 }
