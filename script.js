e7e4a2018774b207b96766c4dd80d9ec8a71984d
matt <mattrfk@gmail.com>
2019-07-26 15:59:31 -0700

    tabs -> spaces

--- script.js
+++ script.js
@@ -9,221 +9,221 @@ const unsolvedColor = "rgba(255, 29, 5, 0.61)"
 const defaultColor = "#d1d1d1"
 
 window.onload = function() {
-	generateGrid(gebi('grid'))
+  generateGrid(gebi('grid'))
 
-	gebi('print').onclick = printTxt
-	gebi('read').onclick = readTxt
-	gebi('load').onclick = () => { 
-		setGrid(sample) 
-		setSolveButtonColor(defaultColor)
-	}
-	gebi('solve').onclick = solveButton
-	gebi('clear').onclick = clear
-	gebi('advanced').onclick = toggleAdvanced
+  gebi('print').onclick = printTxt
+  gebi('read').onclick = readTxt
+  gebi('load').onclick = () => { 
+    setGrid(sample) 
+    setSolveButtonColor(defaultColor)
+  }
+  gebi('solve').onclick = solveButton
+  gebi('clear').onclick = clear
+  gebi('advanced').onclick = toggleAdvanced
 
-	document.addEventListener('keydown', keypress)
+  document.addEventListener('keydown', keypress)
 }
 
 function clear() {
-	g = Array(9).fill(Array(9).fill(' '))
-	setGrid(g)
-	setSolveButtonColor(defaultColor)
+  g = Array(9).fill(Array(9).fill(' '))
+  setGrid(g)
+  setSolveButtonColor(defaultColor)
 }
 
 function toggleAdvanced() {
-	let style = gebi('advanced-holder').style
-	if( style.visibility === "hidden" || style.visibility === "") {
-		style.visibility = "visible"
-	}
-	else {
-		 style.visibility = "hidden"
-	}
+  let style = gebi('advanced-holder').style
+  if( style.visibility === "hidden" || style.visibility === "") {
+    style.visibility = "visible"
+  }
+  else {
+     style.visibility = "hidden"
+  }
 }
 
 function setSolveButtonColor(color=defaultColor){
-	let button = gebi("solve")
-	button.style.backgroundColor = color
+  let button = gebi("solve")
+  button.style.backgroundColor = color
 }
 
 function solveButton() {
-	let grid = getGrid()
-	grid = ss.solve(grid)
-	setGrid(grid)
-	if(ss.isSolved(grid) && ss.isValid(grid)) {
-		setSolveButtonColor(solvedColor)
-	}
-	else {
-		setSolveButtonColor(unsolvedColor)
-	}
+  let grid = getGrid()
+  grid = ss.solve(grid)
+  setGrid(grid)
+  if(ss.isSolved(grid) && ss.isValid(grid)) {
+    setSolveButtonColor(solvedColor)
+  }
+  else {
+    setSolveButtonColor(unsolvedColor)
+  }
 }
 
 function printTxt() {
-	let g = getGrid()
-	let s = ""
-	for(let i = 0; i < 9; i++) {
-		for(let n = 0; n < 9; n++) {
-			if( ss.isBlank(g[i][n])) { 
-				s += '0' 
-			}
+  let g = getGrid()
+  let s = ""
+  for(let i = 0; i < 9; i++) {
+    for(let n = 0; n < 9; n++) {
+      if( ss.isBlank(g[i][n])) { 
+        s += '0' 
+      }
 
-			else { s += g[i][n] }
-			if(n < 8) s += ' '
-		}
+      else { s += g[i][n] }
+      if(n < 8) s += ' '
+    }
 
-		if(i < 8) s += "\n"
-	}
+    if(i < 8) s += "\n"
+  }
 
-	let ta = gebi('text')
-	ta.value = s
+  let ta = gebi('text')
+  ta.value = s
 }
 
 function readTxt() {
-	let ta = gebi('text')
-	let button = gebi('read')
-	let s = ta.value
+  let ta = gebi('text')
+  let button = gebi('read')
+  let s = ta.value
 
-	let grid = s.split('\n')
-	for(i in grid) {
-		grid[i] = grid[i].split(' ')
-	}
+  let grid = s.split('\n')
+  for(i in grid) {
+    grid[i] = grid[i].split(' ')
+  }
 
-	if(ss.isValid(grid)) { 
-		setGrid(grid) 
-		setSolveButtonColor(defaultColor)
-	}
-	else { 
-		//alert("bad data") 
-	}
+  if(ss.isValid(grid)) { 
+    setGrid(grid) 
+    setSolveButtonColor(defaultColor)
+  }
+  else { 
+    //alert("bad data") 
+  }
 }
 
 function keypress(event) {
-	let c = document.activeElement
+  let c = document.activeElement
 
-	if (c.tagName !== 'INPUT') { return }
+  if (c.tagName !== 'INPUT') { return }
 
-	let id = c.getAttribute('id').split('')
-	let x = Number(id[0])
-	let y = Number(id[1])
-	let key = event.key
+  let id = c.getAttribute('id').split('')
+  let x = Number(id[0])
+  let y = Number(id[1])
+  let key = event.key
 
-	function tab(shift=false) {
-		if(shift) {	
-			if(x > 0) { --x }
-			else if(y > 0) { --y; x=8 }
-		} 
-		else {
-			if(x < 8) { ++x }
-			else if(y < 8) { ++y; x=0 }
-		}
-	}
+  function tab(shift=false) {
+    if(shift) {  
+      if(x > 0) { --x }
+      else if(y > 0) { --y; x=8 }
+    } 
+    else {
+      if(x < 8) { ++x }
+      else if(y < 8) { ++y; x=0 }
+    }
+  }
 
-	if (/^[1-9]$/.test(key)) {
-		c.value = key
-		tab()
-	}
-	else {
-		let deletes = [ 'Backspace', 'Delete']
-		if (deletes.includes(key)) {
-			if(key === 'Backspace') {
-				tab(true)
-			}
-			c.value = ' '
-		}
-		let blanks = [' ', '0', '-']
-		if( blanks.includes(key) ){
-			tab()
-		}
+  if (/^[1-9]$/.test(key)) {
+    c.value = key
+    tab()
+  }
+  else {
+    let deletes = [ 'Backspace', 'Delete']
+    if (deletes.includes(key)) {
+      if(key === 'Backspace') {
+        tab(true)
+      }
+      c.value = ' '
+    }
+    let blanks = [' ', '0', '-']
+    if( blanks.includes(key) ){
+      tab()
+    }
 
-		switch(key) {
-			case 'ArrowUp':
-				if(y > 0) --y
-				break
-			case 'ArrowRight':
-				if(x < 8) ++x
-				break
-			case 'ArrowDown':
-				if(y < 8) ++y 
-				break
-			case 'ArrowLeft':
-				if(x > 0) --x 
-				break
-			case 'Tab':
-				if(event.shiftKey) {	
-					tab(true)
-				} 
-				else {
-					tab()
-				}
-		}
-	}
+    switch(key) {
+      case 'ArrowUp':
+        if(y > 0) --y
+        break
+      case 'ArrowRight':
+        if(x < 8) ++x
+        break
+      case 'ArrowDown':
+        if(y < 8) ++y 
+        break
+      case 'ArrowLeft':
+        if(x > 0) --x 
+        break
+      case 'Tab':
+        if(event.shiftKey) {  
+          tab(true)
+        } 
+        else {
+          tab()
+        }
+    }
+  }
 
-	let n = gebi(''+x+y)
-	n.focus()
-	event.preventDefault()
+  let n = gebi(''+x+y)
+  n.focus()
+  event.preventDefault()
 }
 
 function generateGrid(grid) {
-	let t = document.createElement('table')
-	grid.appendChild(t)
-	for(let i = 0; i < 9; i++) {
-		let row = document.createElement('tr')
-		t.appendChild(row)
-		for(let n = 0; n < 9; n++) {
-			let td = document.createElement('td')
-			var textArea = document.createElement('input')
-			var newContent = document.createTextNode(' ')
-			textArea.setAttribute('id', ''+n+i)
-			textArea.setAttribute('type', 'tel')
-			textArea.setAttribute('pattern', '/d*')
+  let t = document.createElement('table')
+  grid.appendChild(t)
+  for(let i = 0; i < 9; i++) {
+    let row = document.createElement('tr')
+    t.appendChild(row)
+    for(let n = 0; n < 9; n++) {
+      let td = document.createElement('td')
+      var textArea = document.createElement('input')
+      var newContent = document.createTextNode(' ')
+      textArea.setAttribute('id', ''+n+i)
+      textArea.setAttribute('type', 'tel')
+      textArea.setAttribute('pattern', '/d*')
 
-			textArea.onfocus = function(e) {
-				e.target.select()
-			}
-			textArea.appendChild(newContent)
-			td.appendChild(textArea)
-			row.appendChild(td)
+      textArea.onfocus = function(e) {
+        e.target.select()
+      }
+      textArea.appendChild(newContent)
+      td.appendChild(textArea)
+      row.appendChild(td)
 
-			let thick = '5px '
-			let thin = '1px '
-			let l = (n === 0 || n === 3 || n === 6) ? thick : thin
-			let t = (i === 0 || i === 3 || i === 6) ? thick : thin
-			let b = (i === 8) ? thick : thin
-			let r = (n === 8) ? thick : thin
-			td.setAttribute('style', 'border-width:'+t+r+b+l+';')
-		}
-	}
+      let thick = '5px '
+      let thin = '1px '
+      let l = (n === 0 || n === 3 || n === 6) ? thick : thin
+      let t = (i === 0 || i === 3 || i === 6) ? thick : thin
+      let b = (i === 8) ? thick : thin
+      let r = (n === 8) ? thick : thin
+      td.setAttribute('style', 'border-width:'+t+r+b+l+';')
+    }
+  }
 
-	gebi('00').setAttribute('autofocus', 'true')
+  gebi('00').setAttribute('autofocus', 'true')
 }
 
 function getGrid() {
-	grid = []
-	for(let i = 0; i < 9; i++) {
-		grid.push([])
-		for(let n = 0; n < 9; n++) {
-			val = gebi(''+n+i).value
-			grid[i].push(val)
-		}
-	}
+  grid = []
+  for(let i = 0; i < 9; i++) {
+    grid.push([])
+    for(let n = 0; n < 9; n++) {
+      val = gebi(''+n+i).value
+      grid[i].push(val)
+    }
+  }
 
-	return grid
+  return grid
 }
 
 // put the data into the html
 function setGrid(grid) {
-	if(grid.length !== 9) {
-		l('bad grid')
-		return
-	}
+  if(grid.length !== 9) {
+    l('bad grid')
+    return
+  }
 
-	for(let i = 0; i < 9; i++) {
-		for(let n = 0; n < 9; n++) {
-			val = grid[i][n]
-			if(val.length > 1 || ss.isBlank(val)) {val = ' '}
-			c = gebi(''+n+i)
-			c.value = val
-		}
-	}
+  for(let i = 0; i < 9; i++) {
+    for(let n = 0; n < 9; n++) {
+      val = grid[i][n]
+      if(val.length > 1 || ss.isBlank(val)) {val = ' '}
+      c = gebi(''+n+i)
+      c.value = val
+    }
+  }
 }
 
 
--- sudoku-solver.js
+++ sudoku-solver.js
@@ -4,250 +4,250 @@ function l(msg) { console.log(msg) }
 
 function SudokuSolver() {
 
-	const Interface = {
-		solve: solve,
-		isBlank: isBlank,
-		isValid: isValid,
-		isSolved: isSolved,
-	}
+  const Interface = {
+    solve: solve,
+    isBlank: isBlank,
+    isValid: isValid,
+    isSolved: isSolved,
+  }
 
-	let solveCycles = 0 // for logging
+  let solveCycles = 0 // for logging
 
-	// take a grid that is a 9x9 2D array
-	// with either single digit or blank represented by 
-	// a space ' '
-	function solve(grid) {
-		// initialize with options
-		for(let i = 0; i < 9; i++) {
-			for(let n = 0; n < 9; n++) {
-				if(isBlank(grid[i][n])) {
-					grid[i][n] = '123456789'
-				}
-			}
-		}
+  // take a grid that is a 9x9 2D array
+  // with either single digit or blank represented by 
+  // a space ' '
+  function solve(grid) {
+    // initialize with options
+    for(let i = 0; i < 9; i++) {
+      for(let n = 0; n < 9; n++) {
+        if(isBlank(grid[i][n])) {
+          grid[i][n] = '123456789'
+        }
+      }
+    }
 
-		let t1 = Date.now()
+    let t1 = Date.now()
 
-		grid = solver(grid) // do the magic
+    grid = solver(grid) // do the magic
 
-		t2 = Date.now()
+    t2 = Date.now()
 
-		l('time to solve ' + (t2 - t1) / 1000 + 's')
-		l('tries ' + solveCycles)
-		solveCycles = 0
+    l('time to solve ' + (t2 - t1) / 1000 + 's')
+    l('tries ' + solveCycles)
+    solveCycles = 0
 
-		return solver(grid)
-	}
+    return solver(grid)
+  }
 
-	function isBlank(c) {
-		if(c === ' ' || c === ''  || c === '0' ) { 
-			return true }
-		return false
-	}
+  function isBlank(c) {
+    if(c === ' ' || c === ''  || c === '0' ) { 
+      return true }
+    return false
+  }
 
-	function solver(g) {
-		solveCycles++
-		// success or failure, either way we are done
-		if(!isValid(g)) { return g }	
-			
-		let grid = doEliminations(g)
-		
-		if(!isSolved(grid) && isValid(grid)) { // need to guess
-			let saved = copyGrid(grid) // save state before guessing
-			
-			x = findPossibilities(grid)
-			while(x !== null) {
-				let e = grid[x.i][x.n]
-				
-				// Pop the last possibility and try it,
-				grid[x.i][x.n] = e.slice(-1) 		
-				saved[x.i][x.n] = e.slice(0,-1) // store the remainders in saved.
-				grid = solver(grid)         // Recursively try this guess.
+  function solver(g) {
+    solveCycles++
+    // success or failure, either way we are done
+    if(!isValid(g)) { return g }  
+      
+    let grid = doEliminations(g)
+    
+    if(!isSolved(grid) && isValid(grid)) { // need to guess
+      let saved = copyGrid(grid) // save state before guessing
+      
+      x = findPossibilities(grid)
+      while(x !== null) {
+        let e = grid[x.i][x.n]
+        
+        // Pop the last possibility and try it,
+        grid[x.i][x.n] = e.slice(-1)     
+        saved[x.i][x.n] = e.slice(0,-1) // store the remainders in saved.
+        grid = solver(grid)         // Recursively try this guess.
 
-				// if we got an invalid grid then start over
-				if(!isValid(grid)) { 
-					grid = copyGrid(saved) 
-				}
-				x = findPossibilities(grid)
-			}
-		}
-		
-		// if here, then there are no more guesses to make;
-		// it is either solved or we are stuck
-		return grid
-	}
+        // if we got an invalid grid then start over
+        if(!isValid(grid)) { 
+          grid = copyGrid(saved) 
+        }
+        x = findPossibilities(grid)
+      }
+    }
+    
+    // if here, then there are no more guesses to make;
+    // it is either solved or we are stuck
+    return grid
+  }
 
-	function findPossibilities(grid) {
-		let shortest = 9
-		let coordinates = null
-		for(let i = 0; i < 9; i++) { 
-			for(let n = 0; n < 9; n++) {
-				let len = grid[i][n].length
-				if(len > 1) {
-					if(len === 2) {
-						return {i:i, n:n}
-					}
-					else if( len < shortest ) {
-						shortest = len
-						coordinates = {i:i, n:n}
-					}
-				}
-			}
-		}
-		return coordinates
-	}
+  function findPossibilities(grid) {
+    let shortest = 9
+    let coordinates = null
+    for(let i = 0; i < 9; i++) { 
+      for(let n = 0; n < 9; n++) {
+        let len = grid[i][n].length
+        if(len > 1) {
+          if(len === 2) {
+            return {i:i, n:n}
+          }
+          else if( len < shortest ) {
+            shortest = len
+            coordinates = {i:i, n:n}
+          }
+        }
+      }
+    }
+    return coordinates
+  }
 
-	// for each place with possibilities, 
-	// check its row, column and square and make eliminations
-	function doEliminations(g) {
+  // for each place with possibilities, 
+  // check its row, column and square and make eliminations
+  function doEliminations(g) {
 
-		// eliminate possibilities in el using certainties in arr
-		function elim(arr, el) {
-			if(el.length === 1) { // no more eliminations to make here
-				return el
-			}
-			for(let i=0; i < arr.length; i++) {
-				if(arr[i].length === 1) { // act only when certain
-					el = el.replace(arr[i], '')
-				}
-			}
-			return el
-		}
+    // eliminate possibilities in el using certainties in arr
+    function elim(arr, el) {
+      if(el.length === 1) { // no more eliminations to make here
+        return el
+      }
+      for(let i=0; i < arr.length; i++) {
+        if(arr[i].length === 1) { // act only when certain
+          el = el.replace(arr[i], '')
+        }
+      }
+      return el
+    }
 
-		let grid = copyGrid(g)
-		let oldGrid
-		do { // keep doing eliminations until no progress is made
-			oldGrid = copyGrid(grid)
-			for(let i = 0; i < 9; i++) {
-				for(let n = 0; n < 9; n++) {
-					let val = grid[i][n]
-					val = elim(getSquare(grid, i, n), val) //square
-					val = elim(getColumn(grid, n), val) //column
-					val = elim(grid[i], val) //row
-					grid[i][n] = val
-				}
-			}
-		} while(!gridsEqual(oldGrid, grid)) // nothing has changed, move on
-		return grid
-	}
+    let grid = copyGrid(g)
+    let oldGrid
+    do { // keep doing eliminations until no progress is made
+      oldGrid = copyGrid(grid)
+      for(let i = 0; i < 9; i++) {
+        for(let n = 0; n < 9; n++) {
+          let val = grid[i][n]
+          val = elim(getSquare(grid, i, n), val) //square
+          val = elim(getColumn(grid, n), val) //column
+          val = elim(grid[i], val) //row
+          grid[i][n] = val
+        }
+      }
+    } while(!gridsEqual(oldGrid, grid)) // nothing has changed, move on
+    return grid
+  }
 
-	// return true even if incomplete but everything checks out
-	function isValid(g) {
-		// check for duplicates
-		function checkDup(arr) {
-			let c = ''
-			for(let i=0; i<arr.length; i++) {
-				if(arr[i].length === 1 && !isBlank(arr[i])) { 
-					if(c.includes(arr[i])) { 
-						return false 
-					}
-					c += arr[i]
-				}
-			}
-			return true
-		}
+  // return true even if incomplete but everything checks out
+  function isValid(g) {
+    // check for duplicates
+    function checkDup(arr) {
+      let c = ''
+      for(let i=0; i<arr.length; i++) {
+        if(arr[i].length === 1 && !isBlank(arr[i])) { 
+          if(c.includes(arr[i])) { 
+            return false 
+          }
+          c += arr[i]
+        }
+      }
+      return true
+    }
 
-		// check grid for empties
-		for(let i=0; i<9; i++) { 
-			for(let n=0; n<9; n++) {
-				if(g[i][n].length < 1) { 
-					return false 
-				}
-			}
-		}
+    // check grid for empties
+    for(let i=0; i<9; i++) { 
+      for(let n=0; n<9; n++) {
+        if(g[i][n].length < 1) { 
+          return false 
+        }
+      }
+    }
 
-		for(let i = 0; i < 9; i++) {
-			if(!checkDup(getColumn(g,i))) { 
-				return false
-			}
-			if(!checkDup(g[i])) { 
-				return false 
-			}
-		}
+    for(let i = 0; i < 9; i++) {
+      if(!checkDup(getColumn(g,i))) { 
+        return false
+      }
+      if(!checkDup(g[i])) { 
+        return false 
+      }
+    }
 
-		for(let s in getSquares(g)) {
-			if(!checkDup(s)) { 
-				return false 
-			}
-		}
+    for(let s in getSquares(g)) {
+      if(!checkDup(s)) { 
+        return false 
+      }
+    }
 
-		return true
-	}
+    return true
+  }
 
-	function isSolved(g) {
-		let f = (x,y) => (Number(x) + Number(y))
+  function isSolved(g) {
+    let f = (x,y) => (Number(x) + Number(y))
 
-		for(let i = 0; i < 9; i++) {
-			if(g[i].reduce(f) !== 45) { 
-				return false 
-			}
-			if(getColumn(g, i).reduce(f) !== 45) { 
-				return false 
-			}
-		}
+    for(let i = 0; i < 9; i++) {
+      if(g[i].reduce(f) !== 45) { 
+        return false 
+      }
+      if(getColumn(g, i).reduce(f) !== 45) { 
+        return false 
+      }
+    }
 
-		let sqs = getSquares(g)
-		for(let i in sqs) {
-			if(sqs[i].reduce(f) !== 45) { 
-				return false 
-			}
-		}
+    let sqs = getSquares(g)
+    for(let i in sqs) {
+      if(sqs[i].reduce(f) !== 45) { 
+        return false 
+      }
+    }
 
-		return true
-	}
+    return true
+  }
 
-	function copyGrid(g) {
-		gnew = []
-		for(let i = 0; i < g.length; i++) {
-			gnew.push([])
-			for(let n = 0; n < g.length; n++) {
-				gnew[i].push(g[i][n])
-			}
-		}
-		return gnew
-	}
+  function copyGrid(g) {
+    gnew = []
+    for(let i = 0; i < g.length; i++) {
+      gnew.push([])
+      for(let n = 0; n < g.length; n++) {
+        gnew[i].push(g[i][n])
+      }
+    }
+    return gnew
+  }
 
-	function gridsEqual(g1, g2) {
-		for(let i = 0; i < g1.length; i++) {
-			for(let n = 0; n < g1.length; n++) {
-				if( g1[i][n] !== g2[i][n] ) return false
-			}
-		}
-		return true
-	}
+  function gridsEqual(g1, g2) {
+    for(let i = 0; i < g1.length; i++) {
+      for(let n = 0; n < g1.length; n++) {
+        if( g1[i][n] !== g2[i][n] ) return false
+      }
+    }
+    return true
+  }
 
-	function getColumn(grid, x) {
-		let col = []
-		for(let i = 0; i < 9; i++) {
-			col.push(grid[i][x])
-		}
-		return col
-	}
+  function getColumn(grid, x) {
+    let col = []
+    for(let i = 0; i < 9; i++) {
+      col.push(grid[i][x])
+    }
+    return col
+  }
 
-	function getSquares(g) {
-		let squares = []
-		for(let i = 0; i < 2; i += 3) {
-			for(let n = 0; n < 2; n += 3) {
-				squares.push(getSquare(g, i, n))
-			}
-		}
-		return squares
-	}
+  function getSquares(g) {
+    let squares = []
+    for(let i = 0; i < 2; i += 3) {
+      for(let n = 0; n < 2; n += 3) {
+        squares.push(getSquare(g, i, n))
+      }
+    }
+    return squares
+  }
 
-	function getSquare(grid, i, n) {
-		const x1 = 3 * Math.floor(i/3)
-		const y1 = 3 * Math.floor(n/3)
-		const x2 = 3 + x1
-		const y2 = 3 + y1
+  function getSquare(grid, i, n) {
+    const x1 = 3 * Math.floor(i/3)
+    const y1 = 3 * Math.floor(n/3)
+    const x2 = 3 + x1
+    const y2 = 3 + y1
 
-		let box = []
-		for(let x = x1; x < x2; x++) {
-			for(let y = y1; y < y2; y++) {
-				box.push(grid[x][y])
-			}
-		}
-		return box
-	}
+    let box = []
+    for(let x = x1; x < x2; x++) {
+      for(let y = y1; y < y2; y++) {
+        box.push(grid[x][y])
+      }
+    }
+    return box
+  }
 
-	return Interface
+  return Interface
 }
